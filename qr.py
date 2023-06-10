2e1f2da121da3d96d650561c275810251583bc73
Kelvin <Kelzjr56@gmail.com>
2023-06-10 19:10:28 -0400

    Implemented QR code image for django site address

--- qr.py
+++ qr.py
@@ -1,3 +1,7 @@
 import qrcode
 
+# Make QR code from Django server host address
+image = qrcode.make("https://127.0.0.1:8000")
 
+# Save PNG image OF Django server host address
+image.save("qr.png")
